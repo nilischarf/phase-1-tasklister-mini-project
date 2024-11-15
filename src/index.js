3dcbc0c8a26f23488f1f882ef739c9124e2ebcb1
Nili Scharf <nilischarf@gmail.com>
2024-11-15 10:32:46 +0200

    complete tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const input = document.getElementById("new-task-description");
+  const list = document.getElementById("tasks");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value.trim();
+    if (task !== "") {
+      const item = document.createElement("li");
+      item.textContent = task;
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = " X ";
+      deleteBtn.addEventListener("click", () => {
+        list.removeChild(item);
+      })
+
+      item.appendChild(deleteBtn, " ");
+      list.appendChild(item);
+      input.value = "";
+    }
+  });
 });
